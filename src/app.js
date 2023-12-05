fb418e87886f4440d47ac1c38b3e4101dfb05f8e
achiewe <mamukashviliachi706@gmail.com>
2023-12-05 20:13:18 +0400

    rename project controller file and also add get request in the app.js file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import connect from "./database/mongo.js";
 import dotenv from "dotenv";
+import { getTask } from "./controllers/project-controllers.js";
 
 dotenv.config();
 
@@ -13,5 +14,7 @@ const app = express();
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "app works" });
 });
+
+app.get("/tasks", getTask);
 
 app.listen(3001);
